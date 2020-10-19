2460f83954fa8a7fa003d97a338ff5702a905914
Erick Daniszewski <edaniszewski@gmail.com>
2020-10-19 10:35:19 -0400

    bump: version 0.2.3

--- fastapi_rfc7807/__init__.py
+++ fastapi_rfc7807/__init__.py
@@ -1,7 +1,7 @@
 """"""
 
 __title__ = 'fastapi_rfc7807'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __description__ = 'FastAPI middleware for RFC-7807 compliant error responses'
 __author__ = 'Erick Daniszewski'
 __author_email__ = 'erick@vapor.io'
